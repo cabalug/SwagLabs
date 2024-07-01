05ac344c68d60b6b09ff1c35d10debbcee5e1084
Tester <caba.lugu@outlook.com>
2024-07-01 16:22:03 -0500

    Add test login with invalid password

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -6,4 +6,12 @@ describe('Login', () => {
     cy.get('#login-button').click()
     cy.contains('Products')
   })
+
+  it('Login with invalid password', () => {
+    cy.visit('https://www.saucedemo.com/v1/index.html')
+    cy.get('#user-name').type('standard_user')
+    cy.get('#password').type('invalid_password')
+    cy.get('#login-button').click()
+    cy.contains('Epic sadface: Username and password do not match any user in this service')
+  })
 })
